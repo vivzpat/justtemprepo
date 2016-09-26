f31195f125809c6f98fb3eeef027cb22f6a8552a
DevViv <vivak13@gmail.com>
2016-09-26 16:44:41 -0400

    wrote a working version of hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,7 @@
 import java.util.*;
 
 public class HelloWorld {
-//	public static void main (String[] args){
-//		System.out.print("Hello World !"); 	
-//	}
-//}
-	public static double degrees = 0.0;
-	public static Scanner reader = new Scanner(System.in);
-
-	public static void main(String[] args) {
-		degrees = reader.nextDouble();
-		String a = "2";
-		convertToCelcius();
-		System.out.println(a);
-	}
-
-	public static void convertToCelcius() {
-		degrees = (degrees - 32.0) * 5.0 / 9.0;
+	public static void main (String[] args){
+		System.out.print("Hello World !"); 	
 	}
 }
